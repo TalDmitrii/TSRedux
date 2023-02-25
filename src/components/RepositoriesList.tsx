2acc9e32b0be055269230143fc62b71fe18c49ef
TalDmitrii <30574780+TalDmitrii@users.noreply.github.com>
2023-02-25 15:14:24 +0300

    useTypedSelector & consume data

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,10 +1,32 @@
+import { useState } from "react";
+import { useActions } from "../hooks/useActions";
+import { useTypedSelector } from "../hooks/useTypedSelector";
+
 const RepositoriesList: React.FC = () => {
+    const [term, setTerm] = useState("");
+    const { searchRepositories } = useActions();
+    const { data, loading, error } = useTypedSelector(
+        (state) => state.repositories
+    );
+
+    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        searchRepositories(term);
+    };
+
     return (
         <div>
-            <form action="">
-                <input type="text" />
+            <form onSubmit={onSubmit}>
+                <input
+                    value={term}
+                    onChange={(e) => setTerm(e.target.value)}
+                    type="text"
+                />
                 <button>Search</button>
             </form>
+            {error && <p>{error}</p>}
+            {loading && <p>Loading...</p>}
+            {!error && !loading && data.map((item) => <div>{item}</div>)}
         </div>
     );
 };
