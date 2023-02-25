95fb94cce478160cd35fbc589e337a002b24fdd2
TalDmitrii <30574780+TalDmitrii@users.noreply.github.com>
2023-02-25 14:14:52 +0300

    Add provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,12 @@
 import ReactDOM from "react-dom/client";
+import App from "./components/App";
 
 const el = document.getElementById("root");
 
 const root = ReactDOM.createRoot(el!);
 
-const App = () => {
-    return (
-        <div>
-            <h1>Hi there!</h1>
-        </div>
-    );
+const MyApp = () => {
+    return <App />;
 };
 
-root.render(<App />);
+root.render(<MyApp />);
